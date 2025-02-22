61a3f7c11205645a67ec6b33db7cd63082479973
Klaudiusz321 <claudiuswebdesign@gmail.com>
2025-02-22 07:17:46 +0100

    correct footer

--- my-project/src/components/Footer.tsx
+++ my-project/src/components/Footer.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
 import '../styles/Footer.scss';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
-import "../styles/Footer.scss";
 
 const Footer = () => {
     const year = new Date().getFullYear();
@@ -13,24 +11,24 @@ const Footer = () => {
             <div className="footer__inner">
 
                 <div className="footer__socials">
-                    <a className="footer__socials-item" href='https://www.linkedin.com/' target='_blank'>
+                    <a className="footer__socials-item" href='https://www.linkedin.com/' target='_blank' rel="noopener noreferrer">
                         <LinkedInIcon />
                         LinkedIn
                     </a>
 
-                    <a className="footer__socials-item" href='https://www.instagram.com/' target='_blank'>
+                    <a className="footer__socials-item" href='https://www.instagram.com/' target='_blank' rel="noopener noreferrer">
                         <InstagramIcon />
                         Instagram
                     </a>
 
-                    <a className="footer__socials-item" href='https://www.facebook.com/' target='_blank'>
+                    <a className="footer__socials-item" href='https://www.facebook.com/' target='_blank' rel="noopener noreferrer">
                         <FacebookIcon />
                         Facebook
                     </a>
                 </div>
 
                 <div className='footer__copyright'>
-                    &copy;{year}. Klaudiusz Sroka. All rights reserved.
+                    &copy;{year}. All rights reserved.
                 </div>
             </div>
         </div>
